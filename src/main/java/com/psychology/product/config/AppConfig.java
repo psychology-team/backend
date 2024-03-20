f58957e74c2221f759d5cb7affe19aa9f935ff31
nemeritskyy <85497703+nemeritskyy@users.noreply.github.com>
2024-03-20 08:50:44 +0200

    refactor: Already not used

--- src/main/java/com/psychology/product/config/AppConfig.java
+++ src/main/java/com/psychology/product/config/AppConfig.java
@@ -1,16 +1,8 @@
 package com.psychology.product.config;
 
-import jakarta.validation.Validator;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public Validator validator() {
-        return new LocalValidatorFactoryBean();
-    }
 
 }
