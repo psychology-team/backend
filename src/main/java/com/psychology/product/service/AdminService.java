8a61e5ca0002a249a31e66dea261c322524ec3b7
ahasparian <a.hasparian@gmail.com>
2024-04-01 20:59:54 +0300

    feat: add AdminService

---
+++ src/main/java/com/psychology/product/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.psychology.product.service;
+
+import com.psychology.product.repository.dto.UserDTO;
+
+import java.util.UUID;
+
+public interface AdminService {
+    UserDTO getCurrentClient(UUID clientId);
+}
