bfbcb645ce9edd66df776f453888eaf9a3957441
ahasparian <a.hasparian@gmail.com>
2024-04-01 16:00:08 +0300

    feat: Add testing user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT  INTO users (user_id, first_name, last_name, email, password, phone, enabled, revoked, revoked_timestamp, created_timestamp, authorities)
 VALUES ('2d16fac9-5dea-4995-b921-b2ec47d0fa04', 'Admin', 'Admin', 'admin@test.com', '$2a$10$U5Oz1fzpRWHhdbMabPboxez2seTtM1V3X76nY.hXFSzKP9y6YTG5a', '', true, false, NULL, '2024-03-27T00:00:00Z', '{"ADMIN"}')
 ON CONFLICT DO NOTHING;
+
+INSERT  INTO users (user_id, first_name, last_name, email, password, phone, enabled, revoked, revoked_timestamp, created_timestamp, authorities)
+VALUES ('2d16fac9-5dea-4995-b921-b2ec47d0fa05', 'User', 'User', 'user@test.com', '$2a$10$U5Oz1fzpRWHhdbMabPboxez2seTtM1V3X76nY.hXFSzKP9y6YTG5a', '', true, false, NULL, '2024-03-27T00:00:00Z', '{"USER"}')
+ON CONFLICT DO NOTHING;
+
