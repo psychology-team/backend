0f3fb26da062ad604c064a4bba7bfa9e4bc03426
ahasparian <a.hasparian@gmail.com>
2024-04-01 16:57:22 +0300

    refactor: remove useless imports

--- src/main/java/com/psychology/product/service/impl/UserServiceImpl.java
+++ src/main/java/com/psychology/product/service/impl/UserServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
