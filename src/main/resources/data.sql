975b91545811701a58c4d202d08ac3079511dc68
ahasparian <a.hasparian@gmail.com>
2024-04-02 00:28:53 +0300

    feat: add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,10 @@ VALUES ('2d16fac9-5dea-4995-b921-b2ec47d0fa04', 'Admin', 'Admin', 'admin@test.co
 ON CONFLICT DO NOTHING;
 
 INSERT  INTO users (user_id, first_name, last_name, email, password, phone, enabled, revoked, revoked_timestamp, created_timestamp, authorities)
-VALUES ('2d16fac9-5dea-4995-b921-b2ec47d0fa05', 'User', 'User', 'user@test.com', '$2a$10$U5Oz1fzpRWHhdbMabPboxez2seTtM1V3X76nY.hXFSzKP9y6YTG5a', '', true, false, NULL, '2024-03-27T00:00:00Z', '{"USER"}')
+VALUES (gen_random_uuid(), 'User', 'User', 'user@test.com', '$2a$10$U5Oz1fzpRWHhdbMabPboxez2seTtM1V3X76nY.hXFSzKP9y6YTG5a', '', true, false, NULL, '2024-03-27T00:00:00Z', '{"USER"}')
+ON CONFLICT DO NOTHING;
+
+INSERT  INTO users (user_id, first_name, last_name, email, password, phone, enabled, revoked, revoked_timestamp, created_timestamp, authorities)
+VALUES (gen_random_uuid(), 'User', 'User', 'user@testing.com', '$2a$10$U5Oz1fzpRWHhdbMabPboxez2seTtM1V3X76nY.hXFSzKP9y6YTG5a', '', true, false, NULL, '2024-03-27T00:00:00Z', '{"USER"}')
 ON CONFLICT DO NOTHING;
 
